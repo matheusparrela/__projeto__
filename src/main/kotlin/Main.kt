3342203cfe6b848264ef4cacd1b57846f3c97f96
Matheus M. Parrela <92748188+matheusparrela@users.noreply.github.com>
2022-07-16 20:13:00 -0300

    Commit teste.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
-fun main(args: Array<String>) {
+fun main() {
     println("Hello World!")
+    var nome = "Matheus"
+    println(nome)
+    println("${soma(5, 8)}")
+    println("${fatorial(5)} ")
+
 }
+
+fun soma(a:Int, b:Int):Int{
+    return a + b
+}
+
+fun fatorial(num:Int):Int{
+    var fat:Int = num
+    var len:Int = num
+
+    while(len > 1) {
+        len -= 1
+        fat *= len
+    }
+    return fat
+}
+
